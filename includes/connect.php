cd6bd81bb97b50fa793508f1aa9c767f41c34f8d
sabnit kaur <sabnitkaur01@sabnits-MacBook-Pro.local>
2020-11-15 20:40:39 -0500

    changes

--- includes/connect.php
+++ includes/connect.php
@@ -1,22 +1,25 @@
 <?php 
     $db_dsn = array( 
         'host' => 'localhost',
-        'dbname' => 'idp_profdata',
+        'dbname' => 'Favourite_things',
         'charset' => 'utf8'
     );
 
+
+
     $dsn = 'mysql:'.http_build_query($db_dsn, '', ';');
-
-    //This is the DB credentials
-
     $db_user = 'root';
-    $db_pass = ''; // windows users leave this blank
+    $db_pass = 'root'; // windows users leave this blank
+
+
 
     try{
         $pdo = new PDO($dsn, $db_user, $db_pass);
         // var_dump($pdo);
         // echo (in this case) is almost like console.log
         // echo "you're in! enjoy the show";
+
+        
     } catch (PDOException $exception){
         echo 'Connection Error:'.$exception->getMessage();
         exit();
